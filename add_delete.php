95dd7008e7fa3073c2fd6f9b7f4025c6322d6bba
Aboubaker Sherif <79166878+AboubakerSherif@users.noreply.github.com>
2022-05-02 22:30:18 -0400

    Create add_delete.php

---
+++ add_delete.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * 
+ */
+class add_delete
+{
+	
+	private $con;
+
+	function __construct()
+	{
+		include_once("Database.php");
+		$db = new Database();
+		$this->con = $db->connect();
+	}
+
+	public function getHouseList(){
+		$query = $this->con->query("SELECT `location`, `floor_plan`, `Number_of_bedrooms`, `Additional_features` FROM `admin` WHERE 1");
+		$ar = [];
+		if ($query->num_rows > 0) {
+			while ($row = $query->fetch_assoc()) {
+				$ar[] = $row;
+			}
+			return ['status'=> 202, 'message'=> $ar];
+		}
+		return ['status'=> 303, 'message'=> 'No House'];
+	}
+
+
+}
+
+
+if (isset($_POST['GET_House'])) {
+	$a = new House();
+	echo json_encode($a->getHouseList());
+	exit();
+	
+}
+
+?>
